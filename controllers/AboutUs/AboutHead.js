7f0cd15575c6fda1010ce696edc4c607bc7e4263
djthap <dhananjaythap111@gmail.com>
2022-02-15 14:37:58 +0530

    about us modified

--- controllers/AboutUs/AboutHead.js
+++ controllers/AboutUs/AboutHead.js
@@ -1,8 +1,8 @@
-const AboutHead = require("../../models/aboutHead");
+const AboutCard = require("../../models/aboutuscard");
 
 exports.getHead = async (req, res) => {
   try {
-    const response = await AboutHead.findOne({_id:"62075fedaea454d0db2872c4"})
+    const response = await AboutCard.findOne({_id:"620b6c54ab4f832910c5cba9"})
     if (response) {
       res.send(response);
     }
@@ -13,10 +13,10 @@ exports.getHead = async (req, res) => {
 
 exports.updateHead = async (req, res) => {
   try {
-    const { title, desc, photo } = req.body;
-    const response = await AboutHead.findByIdAndUpdate(
-      "62075fedaea454d0db2872c4",
-      { title, desc, photo },
+    const { title, desc, image } = req.body;
+    const response = await AboutCard.findByIdAndUpdate(
+      "620b6c54ab4f832910c5cba9",
+      { title, desc, image },
       { new: true }
     );
 
@@ -30,10 +30,10 @@ exports.updateHead = async (req, res) => {
 
 exports.addHead = async (req, res) => {
   try {
-    const newAbout = new AboutHead({
+    const newAbout = new AboutCard({
       title: "this is title",
       desc: "this is desc",
-      photo:
+      image:
         "https://res.cloudinary.com/healo/image/upload/v1644649602/sqho9psp193dcg7i3nnl.jpg",
     });
     const response = await newAbout.save();
--- models/aboutuscard.js
+++ models/aboutuscard.js
@@ -3,13 +3,23 @@ const mongoose = require("mongoose");
 const aboutCardSchema = new mongoose.Schema(
   {
     image: {
-      type: String
+      type: String,
+      trim: true,
+      min: 5,
+      max: 150,
+      required: true,
     },
     title:{
         type:String,
+        required: true,
+        min: 10,
+        max: 700,
     },
     desc: {
         type:String,
+        required: true,
+        min: 10
+       
       
     }
   },
